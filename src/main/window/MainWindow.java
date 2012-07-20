26134e559037392aacc12fbe0fb54500daa2a9a7
Cris Stanza <crisstanza@gmail.com>
2012-07-20 12:48:24 -0300

    Added some visual components.

--- src/main/window/MainWindow.java
+++ src/main/window/MainWindow.java
@@ -1,9 +1,23 @@
 package main.window;
 
+import java.awt.Color;
 import java.awt.Dimension;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
 
 public final class MainWindow extends JFrame {
+
+	private final JTextField tfProject = new JTextField();
+	private final JButton btRefresh = new JButton();
+
+	private final JSeparator separator = new JSeparator();
+
+	private final JLabel lbBranch = new JLabel();
+	private final JTextField tfBranch = new JTextField();
 
 	public MainWindow() {
 		init();
@@ -12,13 +26,26 @@ public final class MainWindow extends JFrame {
 	private final void init() {
 		setTitle("See It!");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setPreferredSize(new Dimension(400, 110));
+		getContentPane().setLayout(null);
+		setPreferredSize(new Dimension(402, 120));
 		setLocationRelativeTo(null);
+		setResizable(false);
+		//
+		//
+		tfProject.setEditable(false);
+	        tfProject.setBounds(8, 8, 280, 24);
+	        getContentPane().add(tfProject);
+		//
+		btRefresh.setText("refresh");
+	        btRefresh.setBounds(280+8+8, 8, 96, 24);
+	        getContentPane().add(btRefresh);
+		//
+	        separator.setBounds(0, 24+8+8, 402, 2);
+	        getContentPane().add(separator);
+		//
+		//
 		pack();
-	        setResizable(false);
 		setVisible(true);
-
-		System.out.println("test seven");
 	}
 
 }
