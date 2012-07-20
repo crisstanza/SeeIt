f85c79969032a5811349617da2e1acfc9fc580ac
Cris Stanza <crisstanza@gmail.com>
2012-07-20 16:38:33 -0300

    Force setting look and feel.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,21 @@
 package main;
 
+import javax.swing.UIManager;
+
 import main.window.MainWindow;
 
 public final class Main {
 
-	public static final void main(final String[] args) {
+	public static final void main(final String[] args) throws Exception {
 		final Main main = new Main();
 	}
 
-	private Main() {
+	private Main() throws Exception {
 		init();
 	}
 
-	private final void init() {
+	private final void init() throws Exception {
+		UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
 		final MainWindow mainWindow = new MainWindow();
 	}
 
